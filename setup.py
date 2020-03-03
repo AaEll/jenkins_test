19ab9853dd119d0e3ebbb74ad4426954388f3991
Aaron <aaronelliot27@gmail.com>
2020-03-03 15:39:45 -0500

    unpacked dbxdemo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dbxdemo',
+      version='0.0.1',
+      description='A sample PySpark application',
+      author='Peter Tamisin',
+      author_email='peter.tamisin@databricks.com',
+      packages=['dbxdemo'],
+      zip_safe=False)
